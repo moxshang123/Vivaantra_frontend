4cde757511b53f97356c981eb23f9b4ec6c71edd
moxshang123 <21ce129@charusat.edu.in>
2025-06-26 14:09:48 +0530

    setup bootsrap,toaster and basic folder structure(empty)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'vivaantra_frontend';
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess() {
+    this.toastr.success('Data saved successfully!', 'Success');
+  }
+
+  showError() {
+    this.toastr.error('Something went wrong!', 'Error');
+  }
 }
